e0b30771f8561d25fc7d8efcad87c7fec8cd689a
Owen Evans <owen@bgeek.net>
2015-08-26 13:18:16 +1200

    add more project work

--- CSharp/Invoice Alerts/Hoist Scripts/PostAlertToSlack.js
+++ CSharp/Invoice Alerts/Hoist Scripts/PostAlertToSlack.js
@@ -1,5 +1,14 @@
 'use strict';
+var varables = require('./Varables.js');
 
-module.exports = function(hoistEvent){
-	console.log(hoistEvent);	
+module.exports = function (hoistEvent) {
+    var slack = global.Hoist.connector(varables.SlackConnectorKey);
+    return slack.get('/chat.postMessage', {
+        channel: varables.SlackChannel,
+        text: JSON.stringify(hoistEvent.payload),
+        username: 'hoist-bot'
+    })
+   .then(function (result) {
+       return Hoist.event.raise('slack:message:sent', result);
+   });
 };
